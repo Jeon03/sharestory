27904383a529d5a48fc86506ed37a470581cb0c6
rizademong <zgustj1004@naver.com>
2025-10-17 18:01:57 +0900

    커밋 출석 2

--- sharestory-frontend/src/pages/MyPage.tsx
+++ sharestory-frontend/src/pages/MyPage.tsx
@@ -1,7 +1,7 @@
 import "../css/myPage.css";
 import { useState } from "react";
 import type { User } from "../types/user";
-//커밋 출석
+//커밋 출석 2
 import {Link, Outlet} from "react-router-dom";
 import FavoriteSlider from "../components/favorite/FavoriteSlider.tsx";
 
